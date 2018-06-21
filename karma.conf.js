d722a273ac8ff35abb098bc1df4a86e246d73f34
kehuagao <khmantis@126.com>
2018-06-21 10:52:54 +0800

    test

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-preprocessors : {'jasmine/src/*.js': 'coverage'},
+preprocessors : {'jasmine/src/**/*.js': 'coverage'},
 coverageReporter: {
     type : 'html',
     dir : 'coverage/'
